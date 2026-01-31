dc1be293dc796cb67202381276db03457ed4675d
Pratik <pbpatel1990@yahoo.com>
2026-01-31 16:24:28 -0500

    Convert App to StatefulWidget, add counter instance var and setState on press of floating action button

--- flutter_pics/lib/src/app.dart
+++ flutter_pics/lib/src/app.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  AppState createState() => AppState();
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       home: Scaffold(
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            print('Floating pressed');
+            setState(() {
+              counter += 1;
+            });
           },
         ),
         appBar: AppBar(title: Text('Let\'s see images')),
